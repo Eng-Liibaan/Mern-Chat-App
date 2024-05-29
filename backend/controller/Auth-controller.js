0ff40efc62a3e4344ae59db1c1518bf660616a33
Abdulahi <Eng-Abdulahi@gmail.com>
2024-05-29 07:13:10 +0300

    file

--- backend/controller/Auth-controller.js
+++ backend/controller/Auth-controller.js
@@ -1,13 +1,13 @@
 const GenerateToken = require('../Generate-Token/Generate-Token');
 const User = require('../model/User-Model')
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const SignUp = async (req, res) => {
     let { FullName, Gender, Email, Password } = req.body;
     
     // ProfileGirl = `https://avator.iran.liara.run/public/boy/?Email=${Email}`
     // ProfileBoy = `https://avator.iran.liara.run/public/boy/?Email=${Email}`
-    // let salt = await bcrypt.genSalt(10)
-    // let HashPass = await bcrypt.hash(Password, salt)
+    let salt = await bcrypt.genSalt(10)
+    let HashPass = await bcrypt.hash(Password, salt)
     let CreateNewUser = await User({ FullName, Gender, Email, Password })
     let UserExist = await User.findOne({ Email })
     if (UserExist) return res.json("User Already Exist")
